389f56c232a2cf59b0a51fc6464e3cc3f3320959
kirill belotserkovskiy <steelinside@users.noreply.github.com>
2014-07-11 22:36:23 +0600

    single.php
    Шаблон отдельной страницы или записи

---
+++ single.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php social_buttons();?>
+				<div class = "st_related_posts">
+					<?php related_posts();?>
+				</div>
+				<?php comments_template( '', true ); ?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
